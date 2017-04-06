d539940f1eab7236c6fc3459f85f12d0c2ff8c64
Irineu <irineu@mixd.com.br>
2017-04-06 17:34:57 +0000

    Fix nas rotas

--- src/routes/web.php
+++ src/routes/web.php
@@ -4,17 +4,15 @@ Route::group(['middleware' => ['web'], 'prefix' => config('admin.url'), 'as' => 
     Route::group(['middleware' => ['auth.admin', 'auth.rules']], function () {
         Route::get('banners/trash', ['uses' => 'BannersAdminController@index', 'as' => '.trash']);
         Route::post('banners/restore/{id}', ['uses' => 'BannersAdminController@restore', 'as' => '.restore']);
-        #Route::get('banners/create/{place}', ['uses' => 'BannersAdminController@create', 'as' => '.create']);
-        #Route::get('banners/{banners}/edit/{place}', ['uses' => 'BannersAdminController@edit', 'as' => '.edit']);
+        Route::get('banners/create/{place}', ['uses' => 'BannersAdminController@create', 'as' => '.create']);
+        Route::get('banners/{banners}/edit/{place}', ['uses' => 'BannersAdminController@edit', 'as' => '.edit']);
         Route::resource('banners', 'BannersAdminController', [
             'names' => [
                 'index' => '.index',
-                'create' => '.create',
                 'store' => '.store',
-                'edit' => '.edit',
                 'update' => '.update',
                 'show' => '.show',
-            ], 'except' => ['destroy']]);
+            ], 'except' => ['create', 'edit', 'destroy']]);
         Route::delete('banners/destroy', ['uses' => 'BannersAdminController@destroy', 'as' => '.destroy']);
     });
 });
